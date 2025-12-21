8c8b3d84d3a8f4d83adfd4ba5cf62b88cc2812e7
abrahamvsdev <abrahamvsdev@gmail.com>
2025-12-21 19:08:18 +0100

    first commit

---
+++ src/main/java/com/vuelify_api/VuelifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.vuelify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuelifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vuelify_api/VuelifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuelify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
