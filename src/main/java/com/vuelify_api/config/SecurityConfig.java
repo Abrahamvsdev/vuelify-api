85bfa41804aa707a7e84f9de13d6007e08a3ad71
abrahamvsdev <abrahamvsdev@gmail.com>
2026-01-04 23:38:48 +0100

    Refactor SecurityConfig and update application properties for Hibernate configuration. API WORKS

--- src/main/java/com/vuelify_api/config/SecurityConfig.java
+++ src/main/java/com/vuelify_api/config/SecurityConfig.java
@@ -2,19 +2,20 @@ package com.vuelify_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
+    
+    //TODO must change when token.
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-        .csrf(csrf -> csrf.disable()) //TODO must change when token.
+        .csrf(csrf -> csrf.disable()) 
 
         .authorizeHttpRequests(auth -> auth
             .anyRequest().authenticated()
